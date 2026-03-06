5a0988fb0c6915456c604b0a03a4aca82aa29701
iniyavan5 <rkiniyavan@gmail.com>
2026-03-06 14:50:19 +0530

    Solved on 06-03-2026

---
+++ GeeksForGeeks_Problems/Easy/Max Sum Subarray of size K.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxSubarraySum(int[] arr, int k) {
+        // Code here
+        int n=arr.length;
+        int max_sum=0;
+        for(int i=0;i<k;i++)
+        {
+            max_sum+=arr[i];
+        }
+        int window_sum=max_sum;
+        for(int i=k;i<n;i++)
+        {
+            window_sum+=arr[i]-arr[i-k];
+            max_sum=Math.max(max_sum,window_sum);
+        }
+        return max_sum;
+    }
+}
