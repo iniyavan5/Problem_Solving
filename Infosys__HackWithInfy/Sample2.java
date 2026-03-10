4bec23e35a7f2497cec0848f321149b541f76362
iniyavan5 <rkiniyavan@gmail.com>
2026-03-10 10:15:38 +0530

    Solved on 10-03-2026

--- Infosys__HackWithInfy/Sample2.java
+++ Infosys__HackWithInfy/Sample2.java
@@ -1,28 +1,74 @@
-
-import java.io.*;
 import java.util.*;
 public class Main
 {
-	public static void main(String[] args) throws IOException
+    public static boolean checkstatus(int N,long[] V,int M,long H,int start)
+    {
+        int i=start;long hh=H;
+        while(i<N)
+        {
+            if(M==0)
+            {
+               return false;
+            }
+            if(hh>V[i])
+            {
+                hh-=V[i];
+                i++;
+            }
+            else if(hh==V[i])
+            {
+                hh=H;
+                M--;
+                i++;
+            }
+            else
+            {
+                M--;
+                hh=H;
+            }
+        }
+        return true;
+    }
+	public static void main(String[] args)
 	{
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		int N=Integer.parseInt(br.readLine());
-		int M=Integer.parseInt(br.readLine());
-		long H=Long.parseLong(br.readLine());
-        long[] arr=new long[(int)N];
-        long sum1=0;
-        for(int i=0;i<N;i++)
-        {
-            arr[i]=Long.parseLong(br.readLine());
-            sum1+=arr[i];
-        }
-        long sum=(long)M*H;
-        int count=0;
-        while(sum1>sum && count<N)
-        {
-            sum1-=arr[count];
-            count++;
-        }
-        System.out.print(count);
+            Scanner sc=new Scanner(System.in);
+            int N=sc.nextInt();
+            int M=sc.nextInt();
+            long H=sc.nextLong();
+            long[] V=new long[N];
+            
+            for(int i=0;i<N;i++)
+            {
+                V[i]=sc.nextLong();
+            }
+            int left=0,right=N-1,ans=0;
+            while(left<=right)
+            {
+                int mid=(left+right)/2;
+                if(checkstatus(N,V,M,H,mid))
+                {
+                    ans=mid;
+                    right=mid-1;
+                }
+                else
+                {
+                    left=mid+1;
+                }
+            }
+            System.out.print(ans);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
